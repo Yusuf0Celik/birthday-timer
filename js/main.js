7fed39828af7c6177431c1fd6daaa0968fdf2c8b
Aexer <302801376@student.rocmondriaan.nl>
2022-03-10 17:13:37 +0100

    beetje css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 console.log("main.js loaded");
 
-const submit = document.querySelector(".submit-btn")
-const timerText = document.querySelector(".timer-text")
+const submit = document.querySelector(".submit-btn");
+const timerInput = document.querySelector(".timer-input");
+const timerText = document.querySelector(".timer-text");
+const daySpan = document.querySelector(".day-span");
+const hourSpan = document.querySelector(".hour-span");
+const minuteSpan = document.querySelector(".minute-span");
+const secondSpan = document.querySelector(".second-span");
+// Variabelen
+
+submit.addEventListener("click", function no() {
+  console.log(timerInput.value);
+})
