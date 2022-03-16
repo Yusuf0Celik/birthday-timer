26f9e712a7529cdf6cc5a058a7349e6acf838905
Yusuf Celik <302801376@student.rocmondriaan.nl>
2022-03-16 10:59:03 +0100

    heb input

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 console.log("main.js loaded");
 
-const submit = document.querySelector(".submit-btn");
+const submitBtn = document.querySelector(".submit-btn");
 const timerInput = document.querySelector(".timer-input");
 const timerText = document.querySelector(".timer-text");
 const daySpan = document.querySelector(".day-span");
@@ -9,8 +9,21 @@ const minuteSpan = document.querySelector(".minute-span");
 const secondSpan = document.querySelector(".second-span");
 // Variabelen
 
-submit.addEventListener("click", submit);
+submitBtn.addEventListener("click", submit);
 
 function submit() {
-  
+    const dateObj = new Date(timerInput.value);
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth() + 1;
+    let day = dateObj.getDate();
+    let hour = dateObj.getHours();
+    let minute = dateObj.getMinutes();
+    // calc to d h m s
+    // put in span
 }
+
+function calculateTime() {
+}
+
+const distance = new Date();
+console.log(distance);
