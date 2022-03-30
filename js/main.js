6f8a3e9d5fa62654e067121ac68534b77b3fc3df
Yusuf Celik <302801376@student.rocmondriaan.nl>
2022-03-30 11:23:03 +0200

    timer werkt helemaal

--- js/main.js
+++ js/main.js
@@ -9,49 +9,97 @@ const hourSpan = document.querySelector(".hour-span");
 const minuteSpan = document.querySelector(".minute-span");
 const secondSpan = document.querySelector(".second-span");
 const announcement = document.querySelector(".announcement");
-let interval;
+let timer;
 // Variabelen
 
 submitBtn.addEventListener("click", submit);
 
 function submit() {
-  if (interval > 1) {
-    clearInterval(interval);
-  } else {
-    interval = setInterval(calculateTime, 1000);
+  checkTime(new Date(timerInput.value))
+  // Gets time and gives it to checkTime
+  localStorage.setItem("timer", JSON.stringify(timerInput.value))
+}
+
+start()
+
+function start() {
+  const eventDate = JSON.parse(localStorage.getItem("timer"));
+  // Get our localstorage
+  if (eventDate) {
+    checkTime(new Date(eventDate));
+    // if it exists set it as the evenDate
   }
 }
 
-function calculateTime() {
-  const localTime = localStorage.setItem("timer", JSON.stringify(timerInput.value));
-  let timerParse = JSON.parse(localStorage.getItem("timer"));
-  // if (timerParse === null) {
-    console.log(localTime);
-    const countToDate = new Date(timerInput.value);
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-    const distance = countToDate - new Date();
-    const textSecond = Math.floor((distance % minute) / second);
-    const textMinute = Math.floor((distance % hour) / minute);
-    const textHour = Math.floor((distance % day) / hour);
-    const textDay = Math.floor(distance / day);
-    secondSpan.textContent = textSecond;
-    minuteSpan.textContent = textMinute;
-    hourSpan.textContent = textHour;
-    daySpan.textContent = textDay;
-    if (countToDate < new Date()) {
-      timerText.style.display = "none";
-      announcement.style.display = "block";
-      announcement.textContent = "Het Event is geweest";
-    } else if(countToDate > new Date()) {
-      timerText.classList.remove("hide");
-      timerText.style.display = "block";
-      announcement.style.display = "none";
-    }
-  // } else {
-    // timerParse;
-    // timerInput.value = timerParse
-  // }
+function checkTime(eventDate) {
+  // eventDate = new Date(timerInput.value)
+  clearInterval(timer);
+  // clearinterval so intervals dont overlap
+  const dateNow = new Date();
+  const distance = eventDate - dateNow;
+  // get distance and todays date
+  if (distance > 0) {
+    // If distance is bigger then one
+    startTimer(distance);
+    timerText.classList.remove("hide");
+    announcement.classList.add("hide");
+  } else {
+    timeHasPassed(distance < -86400000);
+    // 86400000ms = 1 day
+  }
+}
+
+function startTimer(distance) {
+  // distance is distance checkTime
+  distance -= 1000;
+  // distance - 1 sec
+  updateTime(distance);
+  timer = setInterval(() => {
+    distance -= 1000;
+    // distance - 1 sec because setInterval takes one second to start
+      if (distance <= 0) {
+        // if distance is less then or equal to 0
+          clearInterval(timer);
+          // stop timer
+          timeHasPassed(false) 
+          // distance < 86400000 = false
+          return;
+          // Dont execute the rest of the function
+        }
+        updateTime(distance);
+        // update time every second
+  }, 1000);
+}
+
+function updateTime(distance) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  // Calculate s, m, h and d
+  const textSecond = Math.floor((distance % minute) / second);
+  const textMinute = Math.floor((distance % hour) / minute);
+  const textHour = Math.floor((distance % day) / hour);
+  const textDay = Math.floor(distance / day);
+  // Calulate s, m, h and d to text
+  secondSpan.textContent = textSecond;
+  minuteSpan.textContent = textMinute;
+  hourSpan.textContent = textHour;
+  daySpan.textContent = textDay;
+  // put it in span
+}
+
+function timeHasPassed(eventDay) {
+  if (eventDay) {
+    // als meer dan 1 dag is geweest van de event
+    announcement.textContent = "Event has passed";
+  } else {
+    // als niet meer dan 1 dag is geweest van de event
+    announcement.textContent = "Event is here";
+  }
+
+  // announcement hide gets removed
+  announcement.classList.remove("hide");
+  // localStorage timer gets removed
+  localStorage.removeItem("timer");
 }
